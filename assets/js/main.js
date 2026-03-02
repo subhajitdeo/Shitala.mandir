afc949ac725e6e5936e1d3eae1c32876469a8211
subhajitdeo <subhajitdeoghoria5@gmail.com>
2026-03-02 10:07:15 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,4 @@
-/*
-	Helios by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-(function($) {
-
+// my code.
 	var	$window = $(window),
 		$body = $('body'),
 		settings = {
@@ -45,35 +38,6 @@
 
 	// Scrolly.
 		$('.scrolly').scrolly();
-
-	// Nav.
-
-		// Button.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
 
 	// Carousels.
 		$('.carousel').each(function() {
@@ -213,5 +177,6 @@
 				});
 
 		});
+
 
 })(jQuery);
